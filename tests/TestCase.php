17c99c6a92c16b1ea419e5eb30d006c653a84e09
Adam Lee <adam@codelabx.ltd>
2024-03-18 02:40:22 +0000

    Update TestCase to setup testbench database and setup package providers

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,6 +2,9 @@
 
 namespace XtendPackages\RESTPresenter\Tests;
 
+use Laravel\Sanctum\SanctumServiceProvider;
+use Spatie\LaravelData\LaravelDataServiceProvider;
+use XtendPackages\RESTPresenter\Models\User;
 use XtendPackages\RESTPresenter\RESTPresenterServiceProvider;
 use Orchestra\Testbench\TestCase as Orchestra;
 
@@ -10,17 +13,29 @@ class TestCase extends Orchestra
     protected function setUp(): void
     {
         parent::setUp();
+
+        $this->loadLaravelMigrations(['--database' => 'testbench']);
+    }
+
+    protected function getEnvironmentSetUp($app)
+    {
+        $app['config']->set('database.default', 'testbench');
+        $app['config']->set('database.connections.testbench', [
+            'driver' => 'sqlite',
+            'database' => ':memory:',
+            'prefix' => '',
+        ]);
+
+        $app['config']->set('data.date_format', 'Y-m-d\TH:i:s.u\Z');
+        $app['config']->set('rest-presenter.resources.user.model', User::class);
     }
 
     protected function getPackageProviders($app)
     {
         return [
+            SanctumServiceProvider::class,
             RESTPresenterServiceProvider::class,
+            LaravelDataServiceProvider::class,
         ];
-    }
-
-    public function getEnvironmentSetUp($app)
-    {
-        config()->set('database.default', 'testing');
     }
 }
